36f366e86d2d25a9a7a0a0d6ab6b734c562174dc
Max Tsao <mt9304@gmail.com>
2017-08-27 16:50:28 -0700

    something is working. remember to remove log4j stuff.

--- src/cpctools/FXMLDocumentController.java
+++ src/cpctools/FXMLDocumentController.java
@@ -36,8 +36,13 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-
+import com.monitorjbl.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -58,9 +63,8 @@ public class FXMLDocumentController implements Initializable
     private Label l_filename;
     @FXML
     private JFXProgressBar m_progressbar;
-    
+
     File selectedFile;
-    
 
     //Handles changing panes for the main menu. 
     @FXML
@@ -100,37 +104,56 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private void browseFile(MouseEvent event)
     {
-            System.out.println("Pressed Browse. ");
-            Node source = (Node) event.getSource();
-            Window mainStage = source.getScene().getWindow();
+        System.out.println("Pressed Browse. ");
+        Node source = (Node) event.getSource();
+        Window mainStage = source.getScene().getWindow();
 
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open Resource File");
-            fileChooser.getExtensionFilters().addAll(
-                    new ExtensionFilter("Excel Files", "*.xlsx"),
-                    new ExtensionFilter("All Files", "*.*"));
-            selectedFile = fileChooser.showOpenDialog(mainStage);
-            if (selectedFile != null)
-            {
-                l_filename.setText(selectedFile.getName());
-                btn_convertfile.setDisable(false);
-                
-            }
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Excel Files", "*.xlsx"),
+                new ExtensionFilter("All Files", "*.*"));
+        selectedFile = fileChooser.showOpenDialog(mainStage);
+        if (selectedFile != null)
+        {
+            l_filename.setText(selectedFile.getName());
+            btn_convertfile.setDisable(false);
+
+        }
     }
-    
+
     @FXML
     private void convertFile(MouseEvent event) throws IOException, InvalidFormatException
     {
         System.out.println("Converting file. ");
         m_progressbar.setDisable(false);
-        
+
         //Remember to make sense of this. Maybe just use file input stream in the browseFile function instead of delcaring so much. 
         //Also, need to add as many jars needed, xmlbeans.2.6.0 too. 
         //InputStream ExcelFileToRead = new FileInputStream(selectedFile);
         //XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
-        XSSFWorkbook wb = new XSSFWorkbook(selectedFile);
-        System.out.println(wb.getSheetAt(0).getRow(0).getCell(0));
-        System.out.println(wb.getSheetAt(1).getRow(1).getCell(1));
+        //XSSFWorkbook wb = new XSSFWorkbook(selectedFile);
+        //System.out.println(wb.getSheetAt(0).getRow(0).getCell(0));
+        //System.out.println(wb.getSheetAt(1).getRow(1).getCell(1));
+        try (
+                InputStream is = new FileInputStream(selectedFile);
+                Workbook workbook = StreamingReader.builder()
+                        .rowCacheSize(100)
+                        .bufferSize(4096)
+                        .open(is))
+        {
+            Sheet sheet = workbook.getSheet("Sheet2");
+            
+                System.out.println(sheet.getSheetName());
+                for (Row r : sheet)
+                {
+                    for (Cell c : r)
+                    {
+                        System.out.println(c.getStringCellValue());
+                    }
+                }
+            
+        }
     }
 
     @Override
